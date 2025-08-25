088aa1fcdd6166d2d54682aec1b0d0aea66aedaf
dododo25 <dmytro.terekhov@outlook.com>
2025-08-26 00:37:57 +0300

    Add a special tweak to console, favicon update

--- src/components/Page.js
+++ src/components/Page.js
@@ -3,6 +3,8 @@ import { useEffect, useRef } from 'react';
 import Background from './Background';
 import Footer from './Footer';
 
+import nothingToSee from '../images/nothing_to_see.webp';
+
 const Page = props => {
   const headerRef      = useRef();
   const mainContentRef = useRef();
@@ -68,6 +70,39 @@ const Page = props => {
     });
 
     resizeObserver.observe(contentElem);
+
+    (function(url){
+      const img = new Image();
+      img.onload = async function() {
+        try {
+          const response = await fetch(url);
+
+          if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+          }
+
+          const buffer = await response.arrayBuffer();
+          const base64String = btoa(
+            new Uint8Array(buffer).reduce((data, byte) => data + String.fromCharCode(byte), '')
+          );
+
+          console.log(
+            '%c ', [
+              'overflow: visible',
+              'font-size: 1px;',
+              'padding: 0px 119px;',
+              'line-height: 200px;',
+              'background-size: contain;',
+              `background: url(data:image/gif;base64,${base64String});`
+            ].join(' ')
+          );
+        } catch (error) {
+          console.error("Error converting remote GIF:", error);
+        }
+      };
+      img.crossOrigin='anonymous';
+      img.src = url;
+    })('https://i.giphy.com/Mykt45IfyC6qc.webp');
   }, []);
 
   return ([
