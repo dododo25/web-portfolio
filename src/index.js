24e200c826ba1de51af5022f88254168b4c11779
dododo25 <dmytro.terekhov@outlook.com>
2025-08-26 11:17:12 +0300

    Minor change

--- src/index.js
+++ src/index.js
@@ -34,3 +34,37 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <RouterProvider router={router} />
 );
+
+(function(url){
+  const img = new Image();
+
+  img.onload = async function() {
+    try {
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const buffer = await response.arrayBuffer();
+      const base64String = btoa(
+        new Uint8Array(buffer).reduce((data, byte) => data + String.fromCharCode(byte), '')
+      );
+
+      console.log(
+        '%c ', [
+          'overflow: visible',
+          'font-size: 1px;',
+          'padding: 0px 119px;',
+          'line-height: 200px;',
+          'background-size: contain;',
+          `background: url(data:image/gif;base64,${base64String});`
+        ].join(' ')
+      );
+    } catch (error) {
+      console.error("Error converting remote GIF:", error);
+    }
+  };
+  img.crossOrigin='anonymous';
+  img.src = url;
+})('https://i.giphy.com/Mykt45IfyC6qc.webp');
